4b830f327eb11e69fe27840437a967a164c0bf09
User <User@DESKTOP-BTKPDIK>
2023-01-31 17:18:03 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Teste_Git_e_GitHub__VStudio_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstname = "João";
+            string lastname = "Santos";
+
+            Console.WriteLine("\n\nQual é o seu nome? " + "\n");
+            Console.WriteLine("Meu nome é {0} {1}", firstname, lastname + "\n\n");
+
+            int idade = 19;
+            Console.WriteLine("\n\nQual é a sua idade? " + "\n");
+            Console.WriteLine("Meu idade é {0} ", idade + "\n\n");
+
+            Console.ReadLine();
+        }
+    }
+}
